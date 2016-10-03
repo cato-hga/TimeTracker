32ddc99ef6de3bfd8bdb2c43bfc0c2e74612c161
cato-hga <jchristopher.cato@gmail.com>
2016-10-02 21:19:56 -0400

    Refactored index page to use Post partial

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do 
   before do
-    user = User.create(email: "jchristopher.cato@gmail.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
-	login_as(user, :scope => :user)
+    @user = User.create(email: "jchristopher.cato@gmail.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+	login_as(@user, :scope => :user)
   end	
   describe 'index' do
   	 before do
@@ -17,8 +17,8 @@ describe 'navigate' do
 	  expect(page).to have_content(/Posts/)
 	end
 	it 'has a lists of posts' do
-	  post1 = Post.create(date: Date.today, rationale: "Post1")
-	  post2 = Post.create(date: Date.today, rationale: "Post2")
+	  post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+	  post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
 	  visit posts_path
 	  expect(page).to have_content(/Post1|Post2/)
 	end
