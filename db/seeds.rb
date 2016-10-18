6efc42fa9828d3a63d7b2cab9a9306764bfb88d4
cato-hga <jchristopher.cato@gmail.com>
2016-10-18 00:05:41 -0400

    Implemented edit functionality for status approval flow.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 @user = User.create(email: "jchristopher.cato@gmail.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
 puts "1 user createed"
+
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+
+puts "1 Admin User createed"
+
 100.times do |post|
 	Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
+
 puts "100 posts have been created"
